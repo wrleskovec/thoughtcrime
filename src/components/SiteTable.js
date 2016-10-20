c348f9cfd410a938d81f83d529ae402e4e788e16
wrleskovec <russell.leskovec@gmail.com>
2016-10-19 21:56:32 -0500

    changes

--- src/components/SiteTable.js
+++ src/components/SiteTable.js
@@ -6,56 +6,93 @@ import moment from 'moment';
 export default class SiteTable extends React.Component {
   constructor(props) {
     super(props);
-    this.sortSites = this.sortSites.bind(this);
+    // Default to Descending Order on timeSpent
     this.state = {
-      sites: this.sortSites(props.sites, 'timeSpent')
+      sortBy: 'timeSpent',
+      order: 1,
+      sites: this.sortSites(props.sites, 'timeSpent', 1)
     };
   }
-  sortSites(sites, sortBy) {
-    return sites.sort((a, b) => b[sortBy] - a[sortBy]);
+  sortSites(sites, sortBy, order) {
+    return sites.sort((a, b) => order * (b[sortBy] - a[sortBy]));
   }
   render() {
     const itemCount = (this.state.sites.length < 10) ? this.state.sites.length : 10;
-    const topTen = this.state.sites.slice(0, itemCount);
+    let columnCount = 0;
+    const finishedColumns = [];
+    let finishedTable;
+    if (!itemCount) {
+      finishedTable = <div>Nothing to Show</div>;
+    } else {
+      const { sites } = this.state.sites;
+      const topTen = sites.slice(0, itemCount);
+      for (const column in sites[0]) {
+        if (sites[0].hasOwnProperty(column)) {
+          columnCount += 1;
+          finishedColumns.push(
+            <Column
+              header={column}
+              cell={c => (
+                <Cell>
+                  {topTen[c.rowIndex].site}
+                </Cell>
+              )}
+              width={200}
+            />
+          );
+        }
+      }
+      finishedTable = (
+        <Table
+          rowsCount={itemCount}
+          rowHeight={30}
+          headerHeight={30}
+          width={columnCount * 200}
+          height={(itemCount + 1) * 30 + 2}
+        >
+          {finishedColumns}
+        </Table>
+      );
+    }
     return (
-      <Table
-        rowsCount={10}
-        rowHeight={30}
-        headerHeight={30}
-        width={600}
-        height={(itemCount + 1) * 30 + 2}
-      >
-        <Column
-          header="Site"
-          cell={c => (
-            <Cell>
-              {topTen[c.rowIndex].site}
-            </Cell>
-          )}
-          width={200}
-        />
-        <Column
-          columnKey="number"
-          header="Visits"
-          cell={c => (
-            <Cell>
-              {topTen[c.rowIndex].visits}
-            </Cell>
-          )}
-          width={200}
-        />
-        <Column
-          header="Time"
-          cell={c => (
-            <Cell>
-              {moment('2015-01-01').startOf('day')
-                .seconds(this.state.sites[c.rowIndex].timeSpent)
-                .format('H:mm:ss')}
-            </Cell>
-          )}
-          width={200}
-        />
-      </Table>
+      finishedTable
+      // <Table
+      //   rowsCount={10}
+      //   rowHeight={30}
+      //   headerHeight={30}
+      //   width={600}
+      //   height={(itemCount + 1) * 30 + 2}
+      // >
+      //   <Column
+      //     header="Site"
+      //     cell={c => (
+      //       <Cell>
+      //         {topTen[c.rowIndex].site}
+      //       </Cell>
+      //     )}
+      //     width={200}
+      //   />
+      //   <Column
+      //     header="Visits"
+      //     cell={c => (
+      //       <Cell>
+      //         {topTen[c.rowIndex].visits}
+      //       </Cell>
+      //     )}
+      //     width={200}
+      //   />
+      //   <Column
+      //     header="Time"
+      //     cell={c => (
+      //       <Cell>
+      //         {moment('2015-01-01').startOf('day')
+      //           .seconds(this.state.sites[c.rowIndex].timeSpent)
+      //           .format('H:mm:ss')}
+      //       </Cell>
+      //     )}
+      //     width={200}
+      //   />
+      // </Table>
     );
   }
 }
