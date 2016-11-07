57d9302ef648dced4a241a8003480e207f645c19
wrleskovec <russell.leskovec@gmail.com>
2016-11-07 16:11:57 -0600

    pagination looks right

--- src/components/SearchSiteDB.js
+++ src/components/SearchSiteDB.js
@@ -7,19 +7,23 @@ export default class SearchSiteDB extends React.Component {
     // Default to Descending Order on timeSpent
     console.log(props);
     this.onHeaderClick = this.onHeaderClick.bind(this);
-    if (props.sites) {
-      this.state = {
-        sortBy: 'action',
-        order: 1,
-        records: this.sortProps(props.sites, 'action', 1)
-      };
+    this.onPageClick = this.onPageClick.bind(this);
+    let records;
+    if (props.sites && props.sites.length > 1) {
+      records = this.sortProps(props.sites, 'action', 1);
     } else {
-      this.state = {
-        sortBy: 'action',
-        order: 1,
-        records: []
-      };
+      records = [];
     }
+    this.state = {
+      sortBy: 'action',
+      order: 1,
+      records,
+      pageN: 0
+    };
+  }
+  onPageClick(e) {
+    const pageN = parseInt(e.target.id.slice(4));
+    this.setState({ pageN });
   }
   onHeaderClick(header) {
     return e => {
@@ -44,25 +48,41 @@ export default class SearchSiteDB extends React.Component {
   }
 
   render() {
-    return (
-      <table className="table table-striped table-bordered">
-        <thead className="thead-inverse">
-          <tr>
-            <th>#</th>
-            <th onClick={this.onHeaderClick('site')}>Site</th>
-            <th onClick={this.onHeaderClick('visits')}>Visits</th>
-            <th onClick={this.onHeaderClick('timeSpent')}>TimeSpent</th>
-            <th onClick={this.onHeaderClick('action')}>Action</th>
-            <th>AdvAction</th>
-          </tr>
-        </thead>
-        <tbody>
-          {this.state.records.map((record, index, array) => (
-            <SiteDBRow id={index} openModal={this.props.openModal} {...record} />
-          ))}
-        </tbody>
+    const PAGE_LIMIT = 12;
+    const { pageN, records } = this.state;
+    const numOfPages = Math.ceil(records.length / PAGE_LIMIT);
+    const pages = [];
+    for (let i = 0; i < numOfPages; i++) {
+      pages.push(<li key={i}><a key={`page${i}`} id={`page${i}`} href="#">{i + 1}</a></li>);
+    }
+    const pagination = <ul className="pagination" onClick={this.onPageClick}>{pages}</ul>;
+    const offset = pageN * PAGE_LIMIT;
+    const currentPage = records.slice(offset, offset + PAGE_LIMIT);
 
-      </table>
+    return (
+      <div id="searchSiteDB">
+        <table className="table table-striped table-bordered">
+          <thead className="thead-inverse">
+            <tr>
+              <th>#</th>
+              <th onClick={this.onHeaderClick('site')}>Site</th>
+              <th onClick={this.onHeaderClick('visits')}>Visits</th>
+              <th onClick={this.onHeaderClick('timeSpent')}>TimeSpent</th>
+              <th onClick={this.onHeaderClick('action')}>Action</th>
+              <th>AdvAction</th>
+            </tr>
+          </thead>
+          <tbody>
+            {currentPage.map((record, index, array) => (
+              <SiteDBRow
+                id={index} openModal={this.props.openModal}
+                offset={offset} {...record}
+              />
+            ))}
+          </tbody>
+        </table>
+        {pagination}
+      </div>
     );
   }
 }
--- src/components/SiteDBRow.js
+++ src/components/SiteDBRow.js
@@ -3,14 +3,16 @@ import moment from 'moment';
 
 export default function SiteDBRow(props) {
   // utilize id that corresponds to index of element in state
-  const { id, site, timeSpent, action, advAction, visits, openModal } = props;
+  const { id, site, timeSpent, action, advAction, visits, openModal, offset } = props;
   const onClickEdit = rowId => e => openModal(rowId);
   return (
-    <tr id={`datarow-${id}`} key={id} >
-      <th scope="row">{id}</th>
+    <tr id={`datarow-${id + offset}`} key={id + offset} >
+      <th scope="row">{id + offset + 1}</th>
       <td>{site}</td>
       <td>{visits}</td>
-      <td>{timeSpent}</td>
+      <td>{moment('2015-01-01').startOf('day')
+            .seconds(timeSpent)
+            .format('H:mm:ss')}</td>
       <td>{action}</td>
       <td>{JSON.stringify(advAction)}</td>
     </tr>
