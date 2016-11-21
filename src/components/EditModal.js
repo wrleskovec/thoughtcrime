99b22e9b5591b5fd9c8705acd3c959b8fd3bb5f8
wrleskovec <russell.leskovec@gmail.com>
2016-11-21 15:24:46 -0600

    changes

--- src/components/EditModal.js
+++ src/components/EditModal.js
@@ -10,28 +10,29 @@ export default class EditModal extends React.Component {
     const { site, action, advAction, modalClicked } = this.props;
     const showStatus = { display: 'none' };
     if (modalClicked) showStatus.display = 'block';
+    console.log(showStatus);
     return (
       <div
         className="modal fade" id="myModal" style={showStatus}
         tabIndex="-1" role="dialog" aria-labelledby="myModalLabel"
       >
-          <div className="modal-dialog" role="document">
-            <div className="modal-content">
-              <div className="modal-header">
-                <button type="button" className="close" data-dismiss="modal" aria-label="Close">
-                  <span aria-hidden="true">&times;</span>
-                </button>
-                <h4 className="modal-title" id="myModalLabel">Domain options: {site}</h4>
-              </div>
-              <div className="modal-body">
-                ...
-              </div>
-              <div className="modal-footer">
-                <button type="button" className="btn btn-default" data-dismiss="modal">Close</button>
-                <button type="button" className="btn btn-primary">Save changes</button>
-              </div>
+        <div className="modal-dialog" role="document">
+          <div className="modal-content">
+            <div className="modal-header">
+              <button type="button" className="close" data-dismiss="modal" aria-label="Close">
+                <span aria-hidden="true">&times;</span>
+              </button>
+              <h4 className="modal-title" id="myModalLabel">Domain options: {site}</h4>
+            </div>
+            <div className="modal-body">
+              ...
+            </div>
+            <div className="modal-footer">
+              <button type="button" className="btn btn-default" data-dismiss="modal">Close</button>
+              <button type="button" className="btn btn-primary">Save changes</button>
             </div>
           </div>
+        </div>
       </div>
     );
   }
