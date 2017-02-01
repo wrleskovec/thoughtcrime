b7ad2b8bbe904a5c4123f493d10792c8664aa09a
wrleskovec <russell.leskovec@gmail.com>
2017-02-01 15:02:36 -0600

    fixed another bug with charts and refactored code to eliminate redundancy

--- src/components/DailyPieChart.js
+++ src/components/DailyPieChart.js
@@ -9,59 +9,29 @@ export default class DailyPieChart extends React.Component {
   constructor(props) {
     super(props);
     const { dailySites, n } = props;
-    if (dailySites && dailySites[0]) {
-      let totalTime = 0;
-      let fiveTotalTime = 0;
-      const numOfSites = (dailySites.length > n) ? n : dailySites.length;
-      const topSites = this.sortProps(dailySites).slice(0, numOfSites);
-      for (let j = 0; j < numOfSites; j += 1) {
-        fiveTotalTime += topSites[j].timeSpent;
-      }
-      for (let i = 0; i < dailySites.length; i += 1) {
-        totalTime += dailySites[i].timeSpent;
-      }
-      topSites.push({ site: 'Other', timeSpent: totalTime - fiveTotalTime, visits: 0 });
-      this.state = {
-        topSites
-      };
-    } else {
-      this.state = {
-        topSites: []
-      };
-    }
+    this.state = { topSites: this.filterChartData(dailySites, n) };
     this.handleChartClick = this.handleChartClick.bind(this);
     this.handleLegendClick = _.debounce(this.handleLegendClick.bind(this), 50);
-    this.dailyPieChart = null;
   }
-
   componentDidMount() {
     const { topSites } = this.state;
-    const ctx = document.getElementById('dailyPieChart');
-    this.dailyPieChart = new Chart(ctx, {
-      type: 'pie',
-      data: this.getData(),
-      animation: { animateScale: true },
-      options: {
-        maintainAspectRatio: false,
-        onClick: this.handleChartClick,
-        legend: {
-          onClick: this.handleLegendClick
-        },
-        tooltips: {
-          callbacks: {
-            label: (tooltipItems, data) => {
-              const secs = data.datasets[0].data[tooltipItems.index];
-              const site = data.labels[tooltipItems.index];
-              const timeElapsed = moment.duration(secs, 'seconds').format('hh:mm', { trim: false });
-              return `${timeElapsed} - ${site}`;
-            }
-          }
-        }
-      }
-    });
+    if (topSites && topSites[0]) {
+      this.createChart(topSites);
+    }
   }
-  componentWillReceiveProps(nextProps) {
-    const { dailySites, n } = nextProps;
+  componentWillUpdate(nextProps, nextState) {
+    if (nextProps !== this.props) {
+      const { dailySites, n } = nextProps;
+      this.setState({ topSites: this.filterChartData(dailySites, n) });
+    }
+    if (nextState !== this.state) {
+      const { topSites } = nextState;
+      console.log('State change detected');
+      console.log(this.state);
+      this.createChart(topSites);
+    }
+  }
+  filterChartData(dailySites, n) {
     if (dailySites && dailySites[0]) {
       let totalTime = 0;
       let fiveTotalTime = 0;
@@ -74,44 +44,40 @@ export default class DailyPieChart extends React.Component {
         totalTime += dailySites[i].timeSpent;
       }
       topSites.push({ site: 'Other', timeSpent: totalTime - fiveTotalTime, visits: 0 });
-      this.setState({ topSites });
+      return topSites;
     }
+    return [];
   }
-  getData() {
-    const { topSites } = this.state;
-    return {
-      labels: topSites.map(record => record.site),
-      datasets: [{
-        data: topSites.map(record => record.timeSpent),
-        backgroundColor: [
-          '#1b9e77',
-          '#d95f02',
-          '#7570b3',
-          '#e7298a',
-          '#66a61e',
-          '#e6ab02',
-          '#a6761d',
-          '#666666'
-        ],
-        hoverBackgroundColor: [
-          '#3bdead',
-          '#fd9444',
-          '#b5b2d7',
-          '#f183bc',
-          '#99de4a',
-          '#fdcf4e',
-          '#dfaa49',
-          '#999'
-        ]
-      }]
-    };
-  }
-  createChart() {
-    const { topSites } = this.state;
+  createChart(sites) {
     const ctx = document.getElementById('dailyPieChart');
     this.dailyPieChart = new Chart(ctx, {
       type: 'pie',
-      data: this.getData(),
+      data: {
+        labels: sites.map(record => record.site),
+        datasets: [{
+          data: sites.map(record => record.timeSpent),
+          backgroundColor: [
+            '#1b9e77',
+            '#d95f02',
+            '#7570b3',
+            '#e7298a',
+            '#66a61e',
+            '#e6ab02',
+            '#a6761d',
+            '#666666'
+          ],
+          hoverBackgroundColor: [
+            '#3bdead',
+            '#fd9444',
+            '#b5b2d7',
+            '#f183bc',
+            '#99de4a',
+            '#fdcf4e',
+            '#dfaa49',
+            '#999'
+          ]
+        }]
+      },
       animation: { animateScale: true },
       options: {
         maintainAspectRatio: false,
@@ -164,9 +130,6 @@ export default class DailyPieChart extends React.Component {
   }
 
   render() {
-    if (this.dailyPieChart) {
-      this.createChart();
-    }
     return (
       <div>
         <canvas id="dailyPieChart" />
